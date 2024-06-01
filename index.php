21b4c24eddb2c15f8ef328663fd6bbe02f115578
AlexxBelous <connectionwithme1@gmail.com>
2024-06-01 06:13:50 +0300

    Extract a PHP Database Class

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 require "functions.php";
 
+$dsn = "mysql:host=mysql;port=3306;dbname=myApp;charset=utf8mb4";
+$userName = "root";
+$userPass = "root";
+$pdo = new PDO($dsn, $userName, $userPass);
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+
 require "router.php";
+
