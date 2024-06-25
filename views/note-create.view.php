76d7f2af53ced4e4791368652cca8032ed6d0b78
AlexxBelous <connectionwithme1@gmail.com>
2024-06-25 12:03:11 +0300

    Introduction to Form Validation

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -13,7 +13,10 @@
                             <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
                             <div class="mt-2">
                                 <textarea id="body" name="body" rows="3"
-                                          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?php echo $_POST['body'] ?? '' ?></textarea>
+                                <?php if (isset($errors['body'])): ?>
+                                    <p style="color: red;"><?php echo $errors['body'] ?></p>
+                                <?php endif; ?>
                             </div>
                             <div class="mt-6 flex items-center justify-end gap-x-6">
                                 <button type="submit"
