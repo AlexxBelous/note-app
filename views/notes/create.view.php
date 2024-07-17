95d3711304db72c237dd18f21a405ed46585c37b
AlexxBelous <connectionwithme1@gmail.com>
2024-07-17 09:38:34 +0300

    Make Your First Service Container

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -5,7 +5,7 @@
 <?php require base_path('views/partials/banner.php') ?>
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <form action="#" method="POST">
+        <form action="/notes" method="POST">
             <div class="space-y-12">
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
